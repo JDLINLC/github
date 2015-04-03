658f8daaf3da1a37a8da3f563e9f1deb867ba23c
JDLIN <2837848465@qq.com>
2015-04-03 21:21:31 +0800

    c

--- c-k/h.c
+++ c-k/h.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
 #include<string.h>
-void h(char *n,int l);
+void h(char *n);
+void p(int start,int end);
+void t(int num);
 int main(void)
 {
+	int start,end;
 	char n[100];
-	int l;
 
-	scanf("%s",n);
-	l=strlen(n);
-	printf("%s,%d\n",n,l);
-	n[l]='\0';
-	h(n,l);
+	printf("Enter a number to start and a number to end(q to quit):\n");
+	while(scanf("%d %d",&start,&end)==2)
+	{
+		p(start,end);
+	}
+
 	return 0;
 }
-void h(char *n,int l)
+
+
+void h(char *n)
 {
 	char t[100];
+	int i,j=0,l;
 
-	int i;
+	l=strlen(n);
+	n[l]='\0';
 	for(i=0;i<l;i++)
 	{
 		t[l-i-1]=n[i];
@@ -25,6 +32,47 @@ void h(char *n,int l)
 	t[l]='\0';
 	if(strcmp(t,n)==0)
 	{
-		printf("%s\n",n);
-	}	
+		printf("%s\t",n);
+	}
 }
+
+
+void p(int start,int end)
+{
+	int i=0;
+
+	if(start>end)
+	{
+		printf("Your enter is wrong!\nPlease try again(q to quit):\n");
+	}
+	else
+	{
+		for(start;start<=end;start++)
+		{
+			t(start);
+		}
+		printf("\nPlease enter again(q to quit:)\n");
+	}
+}
+
+
+void t(int num)
+{
+	char s[100],o[100];
+	int i=0,j;
+
+	s[i]=num%10+48;
+	while(num/10!=0)
+	{
+		i +=1;
+		num /=10;
+		s[i]=num%10+48;
+	}
+	s[i+1]='\0';
+	for(j=0;j<=i;j++)
+	{
+		o[i-j]=s[j];
+	}
+	o[i+1]='\0';
+	h(o);
+}	
