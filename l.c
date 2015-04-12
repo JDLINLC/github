02553f3360c0be2c76481f75df42795552bd84a2
JDLIN <2837848465@qq.com>
2015-04-12 22:12:18 +0800

    c

--- l.c
+++ l.c
@@ -1,55 +1,32 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#define MAX 45
-struct film{
-    char title[MAX];
-    int rating;
-    struct film *next;
-};
 int main(void)
 {
-        struct film *head=NULL;
-        struct film *current,*prev;
-        char input[MAX];
+    int n,i=0;
+    char ch;
 
-        printf("Enter the movie title(empty line to quit):\n");
-    while(fgets(input,sizeof(input),stdin)!=NULL&&input[0]!='\n')
+    scanf("%d",&n);
+    while(i<n)
     {
-        current=(struct film *)malloc(sizeof(struct film));
-        if(head==NULL)
+        getchar();
+        ch=getchar();
+        if(ch>='0'&&ch<='9')
         {
-            head=current;
-            printf("hello\n");
+            printf("%d",ch-'0');
+        }
+        else if(ch>='a'&&ch<='f')
+        {
+            printf("%d",ch-'a'+10);
+        }
+        else if(ch>='A'&&ch<='F')
+        {
+            printf("%d",ch-'A'+10);
         }
         else
-            prev->next=current;
-        strcpy(current->title,input);
-        printf("Enter the rating:\n");
-        scanf("%d",&current->rating);
-        while(getchar()!='\n')
-            continue;
-        printf("Enter next movie title(empty line to quit):\n");
-        prev=current;
+        {
+            printf("%d",ch);
+        }
+        printf("\n");
+        i++;
     }
-        if(head==NULL)
-        printf("No data entered.\n");
-    else
-        printf("Here is the movie list:\n");
-    current=head;
-    while(current!=NULL)
-    {
-        printf("Movie:%s Rating:%d\n",current->title,current->rating);
-        current=current->next;
-    }
-
-    current=head;
-    while(current!=NULL)
-    {
-            prev=current;
-            free(current);
-            current=prev->next;
-    }
-    printf("Bye!\n");
-        return 0;
+    return 0;
 }
