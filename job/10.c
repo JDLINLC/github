229f5f71a0c391f471a61366c1cec18eb8af9ec3
JDLIN <2837848465@qq.com>
2015-04-20 21:26:23 +0800

    test.c

---
+++ job/10.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,i;
+	char name;
+	float A,B,C,mid;
+
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%f%f%f",&A,&B,&C);
+		if(A>B)
+		{
+			if(A>C)
+			{
+				if(B>C)
+				{
+					mid=B;
+					name='B';
+				}
+				else
+				{
+					mid=C;
+					name='C';
+				}
+			}
+			else
+			{
+				mid=C;
+				name='C';
+			}
+		}
+		else
+		{
+			if(B>C)
+			{
+				if(C>A)
+				{
+					mid=C;
+					name='C';
+				}
+				else
+				{
+					mid=A;
+					name='A';
+				}
+			}
+			else
+			{
+				mid=B;
+				name='B';
+			}
+		}
+		printf("%c:%.1f\n",name,mid);
+	}
+
+	return 0;
+}
