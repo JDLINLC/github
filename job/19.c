0a0b763785d9a390089ecd3e49141f41512b3daa
JDLIN <2837848465@qq.com>
2015-04-23 21:06:35 +0800

    19.c

---
+++ job/19.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void fb(int x);
+int main(void)
+{
+	int x;
+
+	do
+	{
+		scanf("%d",&x);
+		if(x<3)
+		  break;
+		else
+		  fb(x);
+		printf("\n");
+	}while(x!=00);
+
+	return 0;
+}
+void fb(int x)
+{
+	int num[x],i;
+	int j=0,k=0;
+
+	num[0]=1;
+	num[1]=1;
+
+	for(i=2;i<=x;i++)
+	{
+		num[i]=num[i-1]+num[i-2];
+	}
+
+	while(j<x)
+	{
+		printf("%10d",num[j]);
+		k++;
+		if(k==8)
+		{
+		  putchar('\n');
+		  k=0;
+		}
+		j++;
+	}
+}
