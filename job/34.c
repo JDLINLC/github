fc2737aae157a7c7fa8c48a5afd74aad0bee1bad
JDLIN <2837848465@qq.com>
2015-05-05 10:33:42 +0800

    34

--- job/34.c
+++ job/34.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
+#define BALLVOL(r) (3.1415926*r*r*r*4/3)
+
 int main (void)
 {
+	int start,end;
 
+	scanf("%d%d",&start,&end);
+
+	while(start>=0&&end>=0)
+	{
+		printf("radius    volume\n");
+		for(;start<=end;start++)
+		{
+			printf("%d     %lf\n",start,BALLVOL(start));
+		}
+		putchar('\n');
+		scanf("%d%d",&start,&end);
+	}
+
+	return 0;
 }
