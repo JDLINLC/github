a3b367c45d0c5132d58100cfd97cd873b922d7c0
JDLIN <2837848465@qq.com>
2015-05-26 22:55:49 +0800

    52

--- job/52.c
+++ job/52.c
@@ -2,14 +2,15 @@
 #define MAX 81
 int main(void)
 {
-	int n,i=0;
+	int n,i=0,j=0,k=0;
 	
 	scanf("%d",&n);
 
 	while(n!=0)
 	{
-		int k=0;
-		char ch_str[n][MAX],*ptr[MAX],str[n*MAX],*ptr1;
+		getchar();
+		k=0;
+		char ch_str[n][MAX],*ptr[MAX],str[n*MAX],*ptr1,ch;
 
 		for(i=0;i<n;i++)
 			ptr[i]=ch_str[i];
@@ -17,12 +18,18 @@ int main(void)
 
 		for(i=0;i<n;i++)
 		{
-			scanf("%s",ptr[i]);
+			j=0;
+			while((ch=getchar())!='\n')
+			{
+			  ch_str[i][j]=ch;
+			  j++;
+			}
+			ch_str[i][j]='\0';
 		}
 
 		for(i=0;i<n;i++)
 		{
-			int j=0;
+			j=0;
 			while(*(ptr1+k)=*(*(ptr+i)+j))
 			  k++,j++;
 		}
